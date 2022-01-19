637cb12286bee7c9fe3537266c596185b1139dfd
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-01-19 01:38:24 -0700

    Documento base de sintaxis

---
+++ AnalizadorSintaxis/sintaxis.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "cabecera.h"
+
+void SintaxisLeer(){
+}
+
+void SintaxisMostrar(){
+}
+
+void SintaxisMientras(){
+}
+
+void SintaxisPara(){
+}
+
+void SintaxisSi(){
+}
+
+void SintaxisVariables(){
+}
